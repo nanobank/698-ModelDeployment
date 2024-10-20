660cabe721144d03a34406ecc0616995b7250661
nanobank <56763392+nanobank@users.noreply.github.com>
2024-10-20 15:08:57 +0700

    Update app_KPI_65130701934.py

--- app_KPI_65130701934.py
+++ app_KPI_65130701934.py
@@ -14,7 +14,7 @@ import pandas as pd
 import pickle
 
 # Load model and encoders
-with open('model_kpi.pkl', 'rb') as file:
+with open('model_kpi_65130701934.pkl', 'rb') as file:
     model, department_encoder, region_encoder, education_encoder, gender_encoder, recruitment_channel_encoder = pickle.load(file)
 
 # Load your DataFrame
